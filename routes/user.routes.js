bffc3cdeee11eba3f33b55d333fb05eb934fb71e
Dev-Ninja75 <sofiane.ab@gmail.com>
2021-11-08 10:06:28 +0100

    Add profil route and layouts

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,12 +1,12 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-// 2 J'ai appelé le fichier authController pour pouvoir modifier les connection deconnection et s'identifier 
-const authController = require('../controllers/auth.controllers')
+// 2 J'ai appelé le fichier authController pour pouvoir modifier les connection deconnection et s'identifier
+const authController = require("../controllers/auth.controllers");
 
 // 2 Creation de route pour s'inscrire VOIR DOSSIER contollers/auth.controllers.js
-router.get("/register", (req, res, next)=>{
-  res.render('register')
+router.get("/register", (req, res, next) => {
+  res.render("register");
 });
 router.post("/register", authController.signUp);
 
@@ -16,10 +16,12 @@ router.post("/login", authController.signIn);
 // Creation de route pour se deconnecter  VOIR DOSSIER contollers/auth.controllers.js
 router.get("/logout", authController.logout);
 
-// 
-router.get("/:id", authController.getUserPage)
+// Création route User Profil
+router.get("/profil", (req, res) => {
+  res.render("profil");
+});
 
-
-
+//
+router.get("/:id", authController.getUserPage);
 
 module.exports = router;
